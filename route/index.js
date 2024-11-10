8210fb6044e197924f2d754f008755128f8bd37a
Warrier-karthik <karthikwarrier226@gmail.com>
2024-11-10 21:47:20 +0530

    modified:   public/css/main.css 	modified:   route/index.js 	modified:   views/index.ejs

--- route/index.js
+++ route/index.js
@@ -3,11 +3,13 @@ const date = new Date()
 const router = express.Router()
 const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 const Task = require('../model/Task')
-
+const Days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 router.get('/', async (req, res) => {
     const task = new Task()
     const tasks = await Task.find({})
-    res.render('index.ejs', {mnt: month[date.getMonth()]+' '+date.getDate(), task: task, tasks: tasks})
+    let index = 1
+    
+    res.render('index.ejs', {mnt: month[date.getMonth()]+' '+date.getDate(), task: task, tasks: tasks, index:index, day: Days[date.getDay()-1]})
 })
 
 
